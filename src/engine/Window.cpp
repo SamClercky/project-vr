d14ec33e931f166fbd9a79d456497b3e3508de73
SamClercky <SamClercky@users.noreply.github.com>
2022-12-22 22:23:54 +0100

    Activating blending in OpenGL

--- src/engine/Window.cpp
+++ src/engine/Window.cpp
@@ -149,6 +149,10 @@ Window::Window(int width, int height, std::string &&title) {
     // enable Z-buffer
     glEnable(GL_DEPTH_TEST);
 
+    // enable blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // default blending mode
+
     // set input settings
     glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 }
